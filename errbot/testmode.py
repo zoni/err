86d288911e33eebf1e8c3e5c35edf4b0d6dfdd9b
Guillaume BINET <gbin@gootz.net>
2012-06-21 16:23:49 +0200

    more fixes on curses mode

--- errbot/testmode.py
+++ errbot/testmode.py
@@ -10,17 +10,30 @@ curses.curs_set(1)
 screen.keypad(1)
 screen_size_y, screen_size_x = screen.getmaxyx()
 
-mainpad = curses.newpad(120,1000)
-logpad = curses.newpad(120,1000)
+mainpad = curses.newpad(120, 1000)
+logpad = curses.newpad(120, 1000)
 #mypad.border()
 
 mainpad_pos = 0
 logpad_pos = 0
+from pydev import pydevd
+pydevd.settrace('localhost', port=51234, stdoutToServer=False, stderrToServer=False)
 
 def refresh():
-    middle = screen_size_x/2
-    mainpad.refresh(mainpad_pos, 0, 0, 0, screen_size_y-1, middle)
-    logpad.refresh(logpad_pos, 0, 0, middle+1, screen_size_y-1, screen_size_x -1)
+    global mainpad_pos
+    global logpad_pos
+    middle = screen_size_x / 2
+    (y, x) = mainpad.getyx()
+    if mainpad_pos - y > screen_size_y:
+        mainpad_pos = y - screen_size_y
+
+    (y, x) = logpad.getyx()
+    if logpad_pos - y > screen_size_y:
+        logpad_pos = y - screen_size_y
+
+    mainpad.refresh(mainpad_pos, 0, 0, 0, screen_size_y - 1, middle)
+    logpad.refresh(logpad_pos, 0, 0, middle + 1, screen_size_y - 1, screen_size_x - 1)
+
 
 class CursesHandler(Handler):
     def emit(self, record):
@@ -48,26 +61,36 @@ class JIDMock():
 
     def __init__(self, node):
         self.node = node
+
     def getNode(self):
         return self.node
+
     def bareMatch(self, whatever):
         return False
+
     def getStripped(self):
         return self.node
+
 
 class MessageMock():
     def __init__(self, body):
         self.body = body
+
     def getType(self):
         return 'chat'
+
     def getFrom(self):
         return JIDMock(config.BOT_ADMINS[0])
+
     def getProperties(self):
         return {}
+
     def getBody(self):
         return self.body
+
     def getThread(self):
         return None
+
 
 class ConnectionMock():
     def send(self, mess):
@@ -78,6 +101,7 @@ ENCODING_INPUT = sys.stdin.encoding
 
 def prompt():
     mainpad.addstr('\nTalk to me >>')
+
 
 def patch_jabberbot():
     from errbot import jabberbot
@@ -90,7 +114,7 @@ def patch_jabberbot():
         try:
             buffer = ""
             prompt()
-            mainpad_pos=mainpad.getyx()[0] - 40
+            mainpad_pos = mainpad.getyx()[0] - 40
             refresh()
             while True:
                 event = screen.getch()
@@ -106,20 +130,22 @@ def patch_jabberbot():
                 elif event == curses.KEY_BACKSPACE or event == 127:
                     if buffer:
                         buffer = buffer[:-1]
-                        (y,x) = mainpad.getyx()
-                        mainpad.move(y,x-1)
+                        (y, x) = mainpad.getyx()
+                        mainpad.move(y, x - 1)
                         mainpad.delch()
                 elif event == curses.KEY_RESIZE:
                     screen_size_x, screen_size_y = screen.getmaxyx()
-
+                elif event > 256:
+                    mainpad.addstr('MUMBO JUMBO')
                 else:
                     c = str(chr(event))
                     mainpad.addstr(c)
-                    buffer+=c
+                    buffer += c
+
                 refresh()
 
         except EOFError as eof:
-                pass
+            pass
         except KeyboardInterrupt as ki:
             pass
         finally:
